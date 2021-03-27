fc6f630f4c8f40495e9b81534625c6752b48e183
Jacenter <79770295+Jacenter@users.noreply.github.com>
2021-03-27 12:38:01 -0400

    Update Horse.java

--- froilanfarms/Horse.java
+++ froilanfarms/Horse.java
@@ -1,11 +1,19 @@
 package froilanfarms;
 
-public class Horse extends Animal{
+public class Horse extends Animal implements IRideable{
+
     @Override
-
-
-
     public void makeNoise() {
-        System.out.println("NNNEEEYYY");
+        System.out.println("Neeyyyyyyy");
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public boolean isRideable() {
+        return true;
     }
 }
