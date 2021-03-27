238244d3ce0eb9960224e69ab9da696bee7774ea
Jacenter <JNovack1501@gmail.com>
2021-03-27 11:54:18 -0400

    files need to be edited before master approval

--- Froilanda.java
+++ Froilanda.java
@@ -1,4 +1,7 @@
 package froilanfarms;
 
 public class Froilanda extends Person{
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
--- froilanfarms/Animal.java
+++ froilanfarms/Animal.java
@@ -1,6 +1,8 @@
 package froilanfarms;
 
 public abstract class Animal implements IEater, INoiseMaker{
-
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
 }
 
